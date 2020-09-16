c5eede928e6f81eef51bd2c57db45c3de0707bcf
SonamT <sonam.tamang.mr@gmail.com>
2020-09-16 11:18:32 +0600

    lastest commit

--- app.js
+++ app.js
@@ -1,37 +1,92 @@
-// STORE THE INPUT VALUES
-var data = [];
+// TODO CONTROLLER
+var todoController = (function() {
+    var todoArray = [];
 
+    return {
+        addTodoItem: function(item) {
+            todoArray.push(item);
+            
+            var newItem = todoArray.map(function(cur) {
+                return cur;
+            });
 
-//ADD THE ITEMS IN THE UI
-var addItem = function() {
-    var input, item;
-    //Get the input value
-    input = document.querySelector('.todo__input').value;
+            return newItem;
+        },
 
-    //Store in the array
-    data.push(input);
-
-    //Display in the UI
-    if(data.length > 0) {
-        var html, newHtml;
-        html = `<div class="todo__list">
-                    <i class="fas fa-check todo__icon"></i>
-                    %description%
-                </div>`;
-        
-        newHtml = html.replace('%description%', data);
-
-        document.querySelector('.todo__item').insertAdjacentHTML('beforeend', newHtml);
-    }
-}
-
-// SETUP A EVENT LISTENER
-var displayItem = function() {
-    document.addEventListener('keypress', function(e) {
-        if(e.keyCode === 13 || e.which === 13) {
-            addItem();
+        test: function() {
+            return todoArray;
         }
-    });
-}
+    }
 
-displayItem();
+})();
+
+
+// UI CONTROLLER
+var UIController = (function() {
+
+    var DOMstring = {
+        toDoValue: '.todo__input',
+        todoContainer: '.todo__item'
+    };
+
+    return {
+        getInput: function() {
+            return {
+                toDos: document.querySelector(DOMstring.toDoValue).value
+            }
+        },
+
+        displayTodoItem: function(list) {
+            var html, newHtml, element;
+            element = DOMstring.todoContainer;
+
+            html = `<div class="todo__list">
+                        <i class="fas fa-check todo__icon"></i>
+                        %description%
+                    </div>`;
+
+            newHtml = html.replace('%description%', list);
+
+            document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
+        }
+    }
+
+})();
+
+
+// GLOABAL APP CONTROLLER
+var controller = (function(todoCtrl, UICtrl) {
+
+    // Add Todo Item
+    var addItem = function() {
+
+        //1. Get the input field value
+        var input = UICtrl.getInput();
+        console.log(input);
+
+        //2. Add to the todo controller
+        var newItem = todoCtrl.addTodoItem(input.toDos);
+
+        //3. Display in the UI.
+        UICtrl.displayTodoItem(newItem);
+    };
+
+    // Setup Event Listener
+    var setEventListener = function() {
+        document.addEventListener('keypress', function(e) {
+            if(e.keyCode === 13 || e.which === 13) {
+                addItem();
+            }
+        });
+    };
+
+    return {
+        init: function() {
+            setEventListener();
+        }
+    }
+
+})(todoController, UIController);
+
+controller.init();
+
