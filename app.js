c99e7a08101bc9eea232c106c6ba850ba21d3554
SonamT <sonam.tamang.mr@gmail.com>
2020-10-02 11:12:18 +0600

    Migrated to ES6

--- app.js
+++ app.js
@@ -1,54 +1,98 @@
+// ES6 TODO APP
+// ELEMENT LIST 
+let input = document.querySelector('.todo__input');
+let list = document.querySelector('.todo__list');
 
-var form = document.querySelector('.todo__form');
-var input = document.querySelector('.todo__input');
-var list = document.querySelector('.todo__list');
-var item = document.querySelector('.todo__item');
-var button = document.querySelector('#btn--new');
-
-
-//ARRAY FOR STORAGE
-var todosArray = [];
-
-//LOCAL STORAGE FUNCTIONALITY
-var todosArray = localStorage.getItem('todos') ? JSON.parse(localStorage.getItem('todos', todosArray)) : [];
-
-var storage = JSON.parse(localStorage.getItem('todos', todosArray));
-
-//SET EVENT LISTENER
-form.addEventListener('submit', function(e) {
-    e.preventDefault();
-
-    // Add todos in the array
-    todosArray.push(input.value);
-
-    //Store in the storage.
-    localStorage.setItem('todos', JSON.stringify(todosArray));
-
-    //Add new item in UI
-    addTodo(input.value);
-
-    //Clear the input field
-    input.value = '';
-});
-
-// ADD ITEM 
-var addTodo = function(todo) {
-    var newItem, item;
-    item = '<div class="todo__item"><i class="fas fa-check todo__icon"></i>%description%</div>';
-    newItem = item.replace('%description%', todo);
-    list.insertAdjacentHTML('beforeend', newItem)
+/*----------------------------------------------------------------------------
+  IMPLEMENTAION OF LOGIC FOR TODO APP
+-----------------------------------------------------------------------------*/
+//FUNCTION CONSTRUCTOR
+let Activity = function(id, item) {
+    this.id = id;
+    this.item = item;
 };
 
-// DELETE ITEM
-var deleteTodo = function(todo) {
-    
+// DATA STRUCTURES FOR STORING TODO ACTIVITIES
+var data = {
+    todos: {
+        todoItems: []
+    }
+};
+
+//FUNCTION TO ADD A TODO ITEM INTO THE DATA STRUCTURES
+let addTodo = (item) => {
+    let ID, newItem;
+    //Assigning an ID for the todo item.
+    if(data.todos.todoItems.length > 0) {
+        ID = data.todos.todoItems[data.todos.todoItems.length - 1].id + 1;
+    }else {
+        ID = 0;
+    };
+
+    newItem = new Activity(ID, item);
+    data.todos.todoItems.push(newItem);
+
+    return newItem;
+};
+
+/*----------------------------------------------------------------------------
+  DISPLAYING THE TODO ACTIVITIES ON USER INTERFACE
+-----------------------------------------------------------------------------*/
+
+// FUNCTION TO DISPLAY ITEM IN THE UI
+const displayTodo = (todo) => {
+    let html, newHtml;
+
+    html = `<div class="todo__item--container">
+                <div class="todo__item">
+                    <i class="ion-ios-checkmark-empty todo__item-icon"></i>
+                    %activity%
+                </div>
+                <div class="todo__delete">
+                    <button id="btn--delete">Delete</button>
+                </div>
+            <div class="todo__edit">
+                    <button id="btn--edit">Edit</button>
+                </div>
+            </div>`;
+
+    newHtml = html.replace('%activity%',  todo.item);
+
+    list.insertAdjacentHTML('beforeend', newHtml);
 }
 
-for(var i = 0; i < storage.length; i++) {
-    addTodo(storage[i]);
+//FUNCTION TO ADD A TODO ITEM 
+const ctrlAddTodo = () => {
+
+    //1. Get the input values
+    let activity = input.value;
+
+    //2. Add to the data structures
+    let newItem = addTodo(activity);
+
+    //3. Push into the Local Storages
+
+
+    //4. Display in the UI 
+    displayTodo(newItem);
+
+
+    //5. Clear the fields 
 };
 
-// CLEAR OLD AND ADD NEW TODO
-// button.addEventListener('click', function() {
-   
-// });
+
+/*----------------------------------------------------------------------------
+  GLOBAL APP CONTROLLER FOR THE TODO APP
+-----------------------------------------------------------------------------*/
+
+// SETTING EVENT LISTENER(GLOBAL APP CONTROLLER)
+const setEventListener = () => {
+    document.addEventListener('keypress', e => {
+        if(e.keyCode === 13) {
+            ctrlAddTodo();
+        }
+    })
+};
+
+// INITIAZING THE APP
+setEventListener();
